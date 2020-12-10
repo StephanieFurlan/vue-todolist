0cc37431f97ca5086f45c4d999e9474ea2c0a656
Stephanie <stephanie.furlan94@gmail.com>
2020-12-10 12:22:02 +0100

    First Commit - just vue

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var root = new Vue({
+   el: "#root",
+   data: {
+      todoItem: "",
+      todoList: [
+         { text: "Buy milk" },
+         { text: "Do christmas tree" },
+         { text: "Call daddy" },
+         { text: "Clean up room" },
+         { text: "Wash laundery" }
+      ]
+   },
+   methods: {
+      addTodoItem() {
+         this.todoList.push({
+            text: this.todoItem
+         });
+         this.todoItem = "";
+      },
+      deleteTodoItem(index) {
+         this.todoList.splice(index, 1);
+      }
+   }
+})
